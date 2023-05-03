6a7191b7ae6bd7be999e294f3653ca645c69055e
developerjini <i5veagl@gmail.com>
2023-05-03 16:05:53 +0900

    props 변수로 설정

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,9 +1,10 @@
 const MyComponent = (props) => {
+  let { name, children } = props;
   return (
     <div>
       <h1>나의 새롭고 멋진 컴포넌트</h1>
-      <h2>안녕하세요, 제 이름은 {props.name} 입니다.</h2>
-      <h3>children의 값은 {props.children} 입니다.</h3>
+      <h2>안녕하세요, 제 이름은 {name} 입니다.</h2>
+      <h3>children의 값은 {children} 입니다.</h3>
     </div>
   );
 };

09259c2cd544ebcdc4529bc5521a007eba1657bd
developerjini <i5veagl@gmail.com>
2023-05-03 15:58:00 +0900

    props

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,7 +1,10 @@
-import React from "react";
-
-const MyComponent = () => {
-  return <div>나의 새롭고 멋진 컴포넌트</div>;
+const MyComponent = (props) => {
+  return (
+    <div>
+      <h1>나의 새롭고 멋진 컴포넌트</h1>
+      <h2>안녕하세요, 제 이름은 {props.name} 입니다.</h2>
+    </div>
+  );
 };
 
 export default MyComponent;

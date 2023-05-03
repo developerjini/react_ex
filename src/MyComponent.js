d6c77ad664ad02d2e680b586484acca395d9f15e
developerjini <i5veagl@gmail.com>
2023-05-03 16:01:23 +0900

    children

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -3,8 +3,13 @@ const MyComponent = (props) => {
     <div>
       <h1>나의 새롭고 멋진 컴포넌트</h1>
       <h2>안녕하세요, 제 이름은 {props.name} 입니다.</h2>
+      <h3>children의 값은 {props.children} 입니다.</h3>
     </div>
   );
+};
+
+MyComponent.defaultProps = {
+  name: "기본 이름",
 };
 
 export default MyComponent;

89223664931b7e3e8beeddf6000ada522f538a5c
developerjini <i5veagl@gmail.com>
2023-05-08 12:51:16 +0900

    key 응용 - 초기 상태 설정하기

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const IterationSample = () => {
   // let arr = [];
   // arr.map((a, b, c) => "");
-  const names = ["눈사람", "얼음", "눈", "바람"];
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
+  // const names = ["눈사람", "얼음", "눈", "바람"];
+  const obj = [
+    { id: 1, text: "눈사람" },
+    { id: 2, text: "얼음" },
+    { id: 3, text: "눈" },
+    { id: 4, text: "바람" },
+  ];
+
+  const [names, setNames] = useState(obj);
+  const [inputText, setInputText] = useState("");
+  const [nextId, setNextId] = useState(5);
+  const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
   return <ul>{nameList}</ul>;
 };
 

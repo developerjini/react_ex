d0762796277da75394d582f28c94cf803d00908b
developerjini <i5veagl@gmail.com>
2023-05-08 12:45:30 +0900

    key 설정

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -4,7 +4,7 @@ const IterationSample = () => {
   // let arr = [];
   // arr.map((a, b, c) => "");
   const names = ["눈사람", "얼음", "눈", "바람"];
-  const nameList = names.map((name) => <li>{name}</li>);
+  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
   return <ul>{nameList}</ul>;
 };
 

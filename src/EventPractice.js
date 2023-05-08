7599a13c15b60e6fb43d6f080a3548150aa704a5
developerjini <i5veagl@gmail.com>
2023-05-08 10:18:22 +0900

    state에 input 값 담기

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,19 +1,44 @@
-import React from "react";
+// import React from "react";
 
-const EventPractice = () => {
-  return (
-    <div>
-      <h1>이벤트 연습</h1>
-      <input
-        name="message"
-        placeholder="아무거나 입력해보세요"
-        onChange={(e) => {
-          console.log(e);
-          console.log(e.target.value);
-        }}
-      ></input>
-    </div>
-  );
-};
+// const EventPractice = () => {
+
+//   return (
+//     <div>
+//       <h1>이벤트 연습</h1>
+//       <input
+//         name="message"
+//         placeholder="아무거나 입력해보세요"
+//         onChange={(e) => {
+//           console.log(e);
+//           console.log(e.target.value);
+//         }}
+//       ></input>
+//     </div>
+//   );
+// };
+
+// export default EventPractice;
+import React, { Component } from "react";
+
+class EventPractice extends Component {
+  state = { message: "" };
+  render() {
+    return (
+      <div>
+        <h1>이벤트 연습</h1>
+        <input
+          name="message"
+          placeholder="아무거나 입력해보세요"
+          value={this.state.message}
+          onChange={(e) => {
+            this.setState({
+              message: e.target.value,
+            });
+          }}
+        />
+      </div>
+    );
+  }
+}
 
 export default EventPractice;
